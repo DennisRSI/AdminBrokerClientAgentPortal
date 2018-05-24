92b05f6293f29fbc5ef5cf62e61b491cc627edc4
Patrick <patricksheedy@gmail.com>
2018-05-24 16:09:56 -0700

    Code formatting

--- ClientPortal/Models/ApplicationUser.cs
+++ ClientPortal/Models/ApplicationUser.cs
@@ -1,68 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace ClientPortal.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
         private string _mobilePhone = "", _officePhone = "", _officeExt = "", _fax = "", _faxExt = "";
 
-        public ApplicationUser()
-            : base()
-        {
-
-        }
-
-        public ApplicationUser(string companyName, string ein, string firstName, string middleName, string lastName, string address
-            , string city, string state, string postalCode, string country, string mobilePhone, string officePhone
-            , string officeExt, string fax, string faxExt, string email, int brokerId, int clientId, int agentId, string parentId, string creatorIP = "127.0.0.1")
-        {
-            CompanyName = companyName;
-            FirstName = firstName;
-            MiddleName = middleName;
-            LastName = lastName;
-            Address = address;
-            City = city;
-            State = state;
-            PostalCode = postalCode;
-            Country = country;
-            Email = email;
-            OfficePhone = officePhone;
-            OfficeExtension = officeExt;
-            MobilePhone = mobilePhone;
-            Fax = fax;
-            FaxExtension = faxExt;
-            CreatorIP = creatorIP;
-            CreationDate = DateTime.Now;
-            EIN = ein;
-            ParentId = parentId;
-            BrokerId = brokerId;
-            ClientId = clientId;
-            AgentId = agentId;
-        }
         [StringLength(450)]
         public string ParentId { get; set; } = "";
+
         [StringLength(256)]
         public string FirstName { get; set; }
+
         [StringLength(256)]
         public string MiddleName { get; set; }
+
         [StringLength(256)]
         public string LastName { get; set; }
+
         [StringLength(500)]
         public string Address { get; set; }
+
         [StringLength(100)]
         public string City { get; set; }
+
         [StringLength(100)]
         public string State { get; set; }
+
         [StringLength(50)]
         public string PostalCode { get; set; }
+
         [StringLength(100)]
         public string Country { get; set; }
+
         [StringLength(50)]
         public string MobilePhone
         {
@@ -70,6 +43,7 @@ namespace ClientPortal.Models
             {
                 return _mobilePhone;
             }
+
             set
             {
                 if (value != null)
@@ -79,6 +53,7 @@ namespace ClientPortal.Models
                 }
             }
         }
+
         [StringLength(50)]
         public string OfficePhone
         {
@@ -86,6 +61,7 @@ namespace ClientPortal.Models
             {
                 return _officePhone;
             }
+
             set
             {
                 if (value != null)
@@ -96,6 +72,7 @@ namespace ClientPortal.Models
                 }
             }
         }
+
         [StringLength(50)]
         public string OfficeExtension
         {
@@ -103,6 +80,7 @@ namespace ClientPortal.Models
             {
                 return _officeExt;
             }
+
             set
             {
                 if (value != null)
@@ -112,6 +90,7 @@ namespace ClientPortal.Models
                 }
             }
         }
+
         [StringLength(50)]
         public string Fax
         {
@@ -119,6 +98,7 @@ namespace ClientPortal.Models
             {
                 return _fax;
             }
+
             set
             {
                 if (value != null)
@@ -128,6 +108,7 @@ namespace ClientPortal.Models
                 }
             }
         }
+
         [StringLength(50)]
         public string FaxExtension
         {
@@ -135,6 +116,7 @@ namespace ClientPortal.Models
             {
                 return _faxExt;
             }
+
             set
             {
                 if (value != null)
@@ -146,16 +128,24 @@ namespace ClientPortal.Models
         }
 
         public DateTime CreationDate { get; set; } = new DateTime();
+
         [StringLength(50)]
         public string CreatorIP { get; set; } = "127.0.0.1";
+
         public DateTime? DeactivationDate { get; set; } = null;
+
         public string DeactivationReason { get; set; } = "";
+
         [StringLength(100)]
         public string CompanyName { get; set; }
+
         [StringLength(500)]
         public string EIN { get; set; }
+
         public int ClientId { get; set; } = 0;
+
         public int AgentId { get; set; } = 0;
+
         public int BrokerId { get; set; } = 0;
 
         public bool IsAdmin
