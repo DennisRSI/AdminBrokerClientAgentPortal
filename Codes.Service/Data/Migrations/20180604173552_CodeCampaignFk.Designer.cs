512f960b95357a52d011cec9c19d7f04b8603631
Patrick <patricksheedy@gmail.com>
2018-06-04 10:53:05 -0700

    Add CodeCampaignFk migration

--- Codes.Service/Data/Migrations/20180604173552_CodeCampaignFk.Designer.cs
+++ Codes.Service/Data/Migrations/20180604173552_CodeCampaignFk.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Codes.Service.Data.Migrations
 {
     [DbContext(typeof(CodesDbContext))]
-    partial class CodesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180604173552_CodeCampaignFk")]
+    partial class CodeCampaignFk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
