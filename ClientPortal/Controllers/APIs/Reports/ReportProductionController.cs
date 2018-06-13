6490a847c91186497f34342c32d101cdc927f5c2
Patrick <patricksheedy@gmail.com>
2018-06-12 17:15:20 -0700

    Add initial report pages

---
+++ ClientPortal/Controllers/APIs/Reports/ReportProductionController.cs
@@ -0,0 +1,40 @@
+﻿using ClientPortal.Models;
+using Codes.Service.Interfaces;
+using Codes.Service.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientPortal.Controllers.APIs
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class ReportProductionController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IReportService _reportService;
+        private readonly IAccountService _accountService;
+        private readonly ICodeService _context;
+
+        public ReportProductionController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager,
+                                            IReportService reportService, IAccountService accountService, ICodeService context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _reportService = reportService;
+            _accountService = accountService;
+            _context = context;
+        }
+
+        [HttpGet("load/{type}")]
+        public async Task<IActionResult> LoadAsync(string type)
+        {
+            return PartialView("Load");
+        }
+    }
+}
