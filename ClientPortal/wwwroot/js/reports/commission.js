957e85262ebed18efb6816f15c86fb1d7b6560c7
Patrick <patricksheedy@gmail.com>
2018-06-28 21:22:59 -0700

    Initial version of Commission Report

---
+++ ClientPortal/wwwroot/js/reports/commission.js
@@ -0,0 +1,61 @@
+﻿COMMISSION = new Commission();
+
+function Commission() {
+    var self = this;
+
+    this.init = function () {
+
+        self.updateControls();
+
+        $('#select-report').change(function () {
+            self.updateControls();
+        });
+
+        $('button.runreport').click(function () {
+            self.getHtml();
+        });
+    }
+
+    this.updateControls = function () {
+        $('.filter').addClass('hidden');
+        var value = $('#select-report option:selected').val();
+        $('#' + value).removeClass('hidden');
+    }
+
+    this.getHtml = function () {
+        var type = $('#select-report').val();
+        var id = $('#select-' + type + ' option:selected').val();
+        var name = $('#select-' + type + ' option:selected').text();
+        var checkOutStart = $('#checkOutStart').val();
+        var checkOutEnd = $('#checkOutEnd').val();
+
+        // TODO: Fill this in
+        var paymentStatus = "1";
+
+        var url = '/api/reportcommission/gethtml';
+
+        var url = [url, type, id, name, paymentStatus, checkOutStart, checkOutEnd].join('/');
+        url = encodeURI(url);
+
+        $("#loader-container").show();
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (data) {
+                $('#result').html(data);
+                $('#start-report').hide();
+                $('#update-report').removeClass('hidden');
+                $("#loader-container").hide();
+                self.initDataTables();
+            },
+            error: function (xhr, resp, text) {
+                console.log(xhr, resp, text);
+            }
+        })
+    }
+
+    this.initDataTables = function () {
+        $('.jambo_table').DataTable();
+    }
+}
---
+++ Codes.Service/ViewModels/Reports/CommissionResultViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codes.Service.ViewModels
+{
+    public class CommissionResultViewModel
+    {
+        public string Type { get; set; }
+        public string AccountName { get; set; }
+        public string QueryType { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime CheckoutStartDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime CheckoutEndDate { get; set; }
+
+        public int? AgentId { get; set; }
+        public int? ClientId { get; set; }
+        public int? BrokerId { get; set; }
+
+        public List<CommissionResultTableViewModel> Tables { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int TotalCards { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int TotalTransactions { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalInternetPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalYouPayPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double TotalMemberSavings { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalCommissionEarned { get; set; }
+
+        public string ReportTime
+        {
+            get { return DateTime.Now.ToString(); }
+        }
+    }
+
+    public class CommissionResultTableViewModel
+    {
+        public string AccountType { get; set; }
+        public string AccountName { get; set; }
+        public string ReportGroupName { get; set; }
+
+        public List<CommissionResultItemViewModel> Items { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int TotalCards { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int TotalTransactions { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalInternetPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalYouPayPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double TotalMemberSavings { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalCommissionEarned { get; set; }
+    }
+
+    public class CommissionResultItemViewModel
+    {
+        public string AccountName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int NumberCards { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int NumberTransaction { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal InternetPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal YouPayPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double MemberSavings { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal CommissionEarned { get; set; }
+
+        public DateTime PaidDate { get; set; }
+    }
+
+}
