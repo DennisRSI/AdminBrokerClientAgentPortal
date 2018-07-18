36c4100533dd0f0f85f924cebd128d0d3158b509
Patrick <patricksheedy@gmail.com>
2018-07-18 12:21:29 -0700

    Add all countries to dropdown

--- ClientPortal/Models/_ViewModels/AddUserViewModel.cs
+++ ClientPortal/Models/_ViewModels/AddUserViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Codes.Service.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ClientPortal.Models._ViewModels
 {
@@ -6,6 +9,14 @@ namespace ClientPortal.Models._ViewModels
     {
         public string UserType { get; set; }
         public int BrokerId { get; set; }
+        
+        public IEnumerable<SelectListItem> Countries
+        {
+            get
+            {
+                return CountryList.GetSelectList();
+            }
+        }
 
         [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
         public decimal CommissionRate { get; set; }
